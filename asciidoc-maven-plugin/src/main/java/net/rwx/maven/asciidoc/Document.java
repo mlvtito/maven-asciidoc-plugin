2a909d68504ffe757468b8498bd9f45cdd841853
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2012-08-17 18:42:55 +0200

    Set Apache Licence v2

--- asciidoc-maven-plugin/src/main/java/net/rwx/maven/asciidoc/Document.java
+++ asciidoc-maven-plugin/src/main/java/net/rwx/maven/asciidoc/Document.java
@@ -1,15 +1,43 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package net.rwx.maven.asciidoc;
 
 /**
- *
+ * A configuration bean for this Maven mojo.
+ * 
  * @author Arnaud Fonce <arnaud.fonce@r-w-x.net>
  */
 public class Document {
 
+    /** Document title. */
     private String title;
+    
+    /** Docuement source path. */
     private String path;
+    
+    /** Document type. Sould be article or book. */
     private String documentType;
+    
+    /** Document backend. Should be one of backend defined in AsciidocBackendSingleton. */
     private String backend;
+    
+    /** Output path for the generated document. */
     private String outputPath;
 
     public String getBackend() {
