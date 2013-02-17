6d513ea7b2e34cb9c47111816821a80ec76eb06a
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2013-02-17 15:20:26 +0100

    Standardize lincense file header

--- src/main/java/net/rwx/maven/asciidoc/backends/AsciidocBackendSingleton.java
+++ src/main/java/net/rwx/maven/asciidoc/backends/AsciidocBackendSingleton.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of "Maven Asciidoc Plugin".
- * 
- * "Maven Asciidoc Plugin" is free software: you can redistribute it and/or modify
+ * Copyright (C) 2013 Room Work eXperience
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * "Maven Asciidoc Plugin" is distributed in the hope that it will be useful,
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with Maven Asciidoc Plugin.  If not, see <http://www.gnu.org/licenses/>
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.rwx.maven.asciidoc.backends;
 
